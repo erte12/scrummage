84ee48a8b605ee26ce5d3fd30c35614b35a01da2
Bartek <bartek.iskrzycki@gmail.com>
2017-10-22 20:17:57 +0200

    Team details view added

--- Scrummage/Controllers/TeamsController.cs
+++ Scrummage/Controllers/TeamsController.cs
@@ -2,16 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http.Routing;
 using System.Web.Mvc;
+using System.Web.Routing;
+using Scrummage.Models;
 
 namespace Scrummage.Controllers
 {
     public class TeamsController : Controller
     {
-        // GET: Teams
+        private readonly ApplicationDbContext _context;
+
+        public TeamsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
             return View();
+        }
+   
+        [Route("teams/{id:regex(\\d)}")]
+        public ActionResult Details(int id)
+        {
+            var team = _context.Teams.SingleOrDefault(t => t.Id == id);
+
+            if (team == null)
+                return HttpNotFound();
+
+            return View(team);
         }
     }
 }
