9a1b6b69a49464795b02ed5b98351afd9bc1c8ad
Bartek <bartek.iskrzycki@gmail.com>
2017-10-22 19:54:56 +0200

    Confirmation before deleting team implmeneted

--- Scrummage/App_Start/BundleConfig.cs
+++ Scrummage/App_Start/BundleConfig.cs
@@ -11,6 +11,7 @@ namespace Scrummage
             bundles.Add(new ScriptBundle("~/bundles/lib").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/bootstrap.js",
+                        "~/Scripts/bootbox.js",
                         "~/Scripts/respond.js",
                         "~/Scripts/toastr.js"
                         ));
