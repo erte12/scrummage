29c6c1f962b0d8e0f643b91c5d33e020bbe28417
Bartek <bartek.iskrzycki@gmail.com>
2017-11-06 22:18:23 +0100

    ScrumTask service layer with update methods

--- Scrummage/Services/IScrumTasksService.cs
+++ Scrummage/Services/IScrumTasksService.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Scrummage.Models;
 
 namespace Scrummage.Services
 {
-    interface IScrumTasksService
+    public interface IScrumTasksService
     {
-
+        void UpdateUser(int userId);
+        void UpdateEstimation(int estimationId);
+        void UpdatePriority(int priority);
+        void UpdateTaskType(TaskType taskType, int? tookId);
     }
 }
