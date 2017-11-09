5f12dd6a60b42cc5efd32e53d92567a4fa8ce878
Bartek <bartek.iskrzycki@gmail.com>
2017-11-09 16:57:14 +0100

    Sprint burndown chart web api defined

---
+++ Scrummage/Controllers/Api/SprintBurndownChartsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace Scrummage.Controllers.Api
+{
+    public class SprintBurndownChartsController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Index()
+        {
+            var csvData = new StringBuilder("Day,Ideal,Left\r\n");
+
+            csvData.Append("3/9/13,5691,4346\r\n");
+            csvData.Append("4/9/13,7086,2300\r\n");
+            csvData.Append("5/9/13,3575,8735\r\n");
+
+            return Ok(csvData.ToString());
+        }
+    }
+}
