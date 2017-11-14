bdaf3af5d5eb20d337e40f8571193d197f97ec0a
Bartek <bartek.iskrzycki@gmail.com>
2017-11-14 15:16:29 +0100

    Events view implemented

---
+++ Scrummage/Controllers/EventController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using Scrummage.Core;
+using Scrummage.Models;
+using Scrummage.ViewModels;
+
+namespace Scrummage.Controllers
+{
+    public class EventController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public EventController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
