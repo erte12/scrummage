17a47b6540cef9cb49ce3b25cb5b780c25a4bfff
Bartek <bartek.iskrzycki@gmail.com>
2017-11-27 20:30:01 +0100

    Teams index view javascript refactoring

--- Scrummage/App_Start/BundleConfig.cs
+++ Scrummage/App_Start/BundleConfig.cs
@@ -41,6 +41,10 @@ namespace Scrummage
                         "~/Scripts/jquery.enter-submit.js",
                         "~/Scripts/jquery.validate*"));
 
+            //Views
+            bundles.Add(new ScriptBundle("~/bundles/views/teams/index").Include(
+                "~/Scripts/Views/Teams/index.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
---
+++ Scrummage/Scripts/Views/Teams/index.js
@@ -0,0 +1,100 @@
+﻿var source = $('#teamRow').html();
+var template = Handlebars.compile(source);
+var table = $('#teamsTable');
+
+function LoadTeamsTableData() {
+    $.get('/Api/Teams', function (data) {
+        data.forEach(function (team) {
+            var teamRowHtml = template(team);
+            $('#teamsTable tbody').append(teamRowHtml);
+        });
+    }).done(function () {
+        table = table.DataTable();
+    });
+}
+
+function BindTeamCreateEvent() {
+    $('#newTeamButton').on('click', function () {
+        bootbox.prompt({
+            size: "small",
+            title: "Team name:",
+            callback: function (name) {
+                if (name !== null) {
+                    $.ajax({
+                            url: '/Api/Teams',
+                            method: 'post',
+                            data: { name: name }
+                        })
+                        .done(function (newTeam) {
+                            toastr.success('New team has been created!');
+                            var teamRowHtml = template(newTeam);
+                            table.row.add($(teamRowHtml)).draw();
+                            //TODO: Validation
+                        })
+                        .fail(function () {
+                            toastr.error('Something went wrong!');
+                        });
+                }
+            }
+        });
+    });
+
+    $('#newTeam').validate({
+        submitHandler: function () {
+            $.ajax({
+                    url: '/Api/Teams',
+                    method: 'post',
+                    data: $('#newTeam').serialize()
+                })
+                .done(function (newTeam) {
+                    toastr.success('New team has been created!');
+                    $('#newTeam').find('input').val('');
+
+                    //TODO: Insert new team to table
+                })
+                .fail(function () {
+                    toastr.error('Something went wrong!');
+                });
+
+            return false;
+        }
+    });
+}
+
+function BindTeamDeleteEvent()
+{
+    $('#teamsTable tbody').on('click', '.js-delete', function () {
+
+        var teamRow = $(this).parents('tr');
+        var teamId = teamRow.attr('data-team-id');
+
+        bootbox.confirm({
+            title: "Delete team?",
+            message: "Do you want to delete this team? This cannot be undone.",
+            buttons: {
+                cancel: {
+                    label: '<i class="fa fa-times"></i> Cancel'
+                },
+                confirm: {
+                    label: '<i class="fa fa-check"></i> Confirm'
+                }
+            },
+            callback: function (result) {
+                if (result === true) {
+                    $.ajax({
+                            url: '/Api/Teams/' + teamId,
+                            method: 'delete'
+                        })
+                        .done(function () {
+                            toastr.success('Team successfully deleted');
+                            table.rows(teamRow).remove().draw();
+                        })
+                        .fail(function () {
+                            toastr.error('Something went wrong');
+                        });
+                }
+            }
+        });
+    });
+}
+
